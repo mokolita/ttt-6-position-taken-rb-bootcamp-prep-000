77e168ce003f396c7d2778ac491727a22598c1de
Meg Galvez <meg@oakmaine.com>
2019-03-10 20:04:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,7 @@ def position_taken?(board, index)
 
 <<<<<<< HEAD
  ! (board[index] == " " || board[index] == "" || board[index] == nil)
-=======
-  board[index]
-   " " || '' == "X" || "O"
-  "X" || "O" == "X" || "O"
+
     
    
 >>>>>>> 08506d7f57eb9391d0aadf97837aac319a4e587b
